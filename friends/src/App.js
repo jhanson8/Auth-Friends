1bd955543edb3d5d81adace119b49847285f3d51
Jonathan Hanson <jhanson8@uoregon.edu>
2020-01-28 17:30:26 -0800

    add friend form post works

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Login from "./components/Login.js";
 import FriendList from "./components/FriendList.js";
+import AddFriend from "./components/FriendForm.js";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
@@ -15,8 +16,12 @@ function App() {
           <li>
             <Link to="/protected">Protected Page</Link>
           </li>
+          <li>
+            <Link to="/protectedAdd">Add Friend</Link>
+          </li>
         </ul>
         <Switch>
+          <PrivateRoute path="/protectedAdd" component={AddFriend} />
           <PrivateRoute path="/protected" component={FriendList} />
           <Route path="/login" component={Login} />
           <Route component={Login} />
